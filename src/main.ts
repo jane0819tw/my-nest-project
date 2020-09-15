2e9ca28a061323f6f70dedcb47e3d1778f2bb357
jane0819tw <jane0819tw@gmail.com>
2020-09-15 15:58:33 +0800

    show hello world

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  let port = 3003;
+  await app.listen(port);
+  console.log(`app is running on: http://localhost:${port}`);
 }
 bootstrap();
