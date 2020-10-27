dd29104bcfe3f0d8bbda6cb182f0f1d84b5a8387
jane0819tw <jane0819tw@gmail.com>
2020-10-28 00:11:46 +0800

    getAllTasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) { }
+  // 要告知使用http的哪一種請求作為decorator，getAllTasks方法才可以被執行=> 加上Get()
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks()
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // 屬性給private, 這樣在DI的時候，才不會讓他被外面的東西修改
+  private tasks = []
+
+  getAllTasks() {
+    return this.tasks;
+  }
+}
