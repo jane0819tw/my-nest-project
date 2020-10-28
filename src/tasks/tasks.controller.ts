5f306750e08da7b8e6e3994c0871097da5862fff
jane0819tw <jane0819tw@gmail.com>
2020-10-28 23:04:15 +0800

    define task model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,12 @@
 import { Controller,Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-
+import { Task } from './task.model';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) { }
   // 要告知使用http的哪一種請求作為decorator，getAllTasks方法才可以被執行=> 加上Get()
   @Get()
-  getAllTasks() {
+  getAllTasks():Task[] {
     return this.tasksService.getAllTasks()
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
   // 屬性給private, 這樣在DI的時候，才不會讓他被外面的東西修改
-  private tasks = []
+  // 再回傳的東西上加上 :Task[]，表示回傳格式
+  private tasks:Task[] = []
 
-  getAllTasks() {
+  getAllTasks():Task[] {
     return this.tasks;
   }
 }
