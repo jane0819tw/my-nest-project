62b8e7f524e4052d20696289ab601897b4d926ca
jane0819tw <jane0819tw@gmail.com>
2020-11-15 11:06:57 +0800

    create a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task ,TaskStatus} from './task.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
   // 屬性給private, 這樣在DI的時候，才不會讓他被外面的東西修改
@@ -9,5 +9,16 @@ export class TasksService {
 
   getAllTasks():Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string):Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    }
+    this.tasks.push(task)
+    return task
   }
 }
