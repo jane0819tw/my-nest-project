f8e1ec409d021b874820bd44f227cf99c40d8fe3
jane0819tw <jane0819tw@gmail.com>
2020-11-15 14:42:10 +0800

    get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Get, Post,Body } from '@nestjs/common';
+import { Controller,Get, Post,Body ,Param} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import {CreateTaskDto} from './dto/create-task.dto'
@@ -11,6 +11,10 @@ export class TasksController {
     return this.tasksService.getAllTasks()
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id:string) {
+    return this.tasksService.getTaskById(id)
+  }
   // 如果透過Body 新增欄位: createTask(@Body('title') title: string)
   @Post()
   createTask(
